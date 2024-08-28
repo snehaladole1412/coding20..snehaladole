9793f51720eb5d3d9c74f5cf6f39d9e531176f82
SnehalAdole1412 <snehaladole2003@gmail.com>
2024-08-28 20:42:28 +0530

    index file is addes for practice of git

--- java program-Snehal Adole/EvenoddCount.java
+++ java program-Snehal Adole/EvenoddCount.java
@@ -5,7 +5,7 @@ public class EvenOddCount
 {
 public static void main(String[] args) 
 {
-int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 System.out.println("Original Array:");
 printArray(array);
         
